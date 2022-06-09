99b284884e4f0b767b8aee483ebc621118d597f9
Gaetano Salvatore Falco <56557552+chowned@users.noreply.github.com>
2022-06-09 15:45:14 +0200

    Update train.sh
    Addes parameters to easily track the hyperparameters experiments that are running

--- Implementation/train.sh
+++ Implementation/train.sh
@@ -3,17 +3,17 @@
 epochs=10
 
 #running code with default parameters
-echo "Running train.py --epoch $epochs --batch_size 64"
+echo "Running 1 -> train.py --epoch $epochs --batch_size 64"
 python3 ./train.py --resume --epoch $epochs --batch_size 64 --data_root ../../datasets_dir/ROD-synROD/
 
 #running code without DA
-echo "Running train.py --epoch $epochs --batch_size 64 --lr 0.0003 --weight_rot 0 --weight_ent 0.1"
+echo "Running 2 -> train.py --epoch $epochs --batch_size 64 --lr 0.0003 --weight_rot 0 --weight_ent 0.1"
 python3 ./train.py --resume --epoch $epochs --batch_size 64 --lr 0.0003 --weight_rot 0 --weight_ent 0.1 --data_root ../../datasets_dir/ROD-synROD/
 
 #running code with higher learning rate
-#echo "Running train.py --epoch $epochs --batch_size 64 --lr 0.0006 --weight_rot 1.0 --weight_ent 0.1"
+#echo "Running 3 -> train.py --epoch $epochs --batch_size 64 --lr 0.0006 --weight_rot 1.0 --weight_ent 0.1"
 #python3 ./train.py --resume --epoch $epochs --batch_size 64 --lr 0.0006 --weight_rot 1.0 --weight_ent 0.1 --data_root ../../datasets_dir/ROD-synROD/
 
 #running code with higher weight entropy
-#echo "Running train.py --resume --epoch $epochs --batch_size 64 --lr 0.0003 --weight_rot 1.0 --weight_ent 0.2"
+#echo "Running 4 -> train.py --resume --epoch $epochs --batch_size 64 --lr 0.0003 --weight_rot 1.0 --weight_ent 0.2"
 #python3 ./train.py --resume --epoch $epochs --batch_size 64 --lr 0.0003 --weight_rot 1.0 --weight_ent 0.2 --data_root ../../datasets_dir/ROD-synROD/
